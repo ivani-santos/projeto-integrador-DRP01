1233f93b0a0a7404499a23b05e49a0521cbb97fd
ivani santos <ivanisandre@outlook.com>
2026-04-26 19:59:31 -0300

    SEPARAÇÃO AMBIENTES - ciente e profissional

--- website/auth.py
+++ website/auth.py
@@ -2,14 +2,22 @@ from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
-def login():
-    return render_template("login.html")
+@auth.route('/login-fotografo')
+def login_fotografo():
+    return render_template("login_fotografo.html")
+
+@auth.route('/login-client')
+def login_client():
+    return render_template("login_client.html")
 
 @auth.route('/logout')
 def logout():
     return "<p>Sair</p>"
 
-@auth.route('/signup')
-def signup():
-    return render_template("signup.html")
+@auth.route('/signup-fotografo')
+def signup_fotografo():
+    return render_template("signup_fotografo.html")
+
+@auth.route('/signup-client')
+def signup_client():
+    return render_template("signup_client.html")
