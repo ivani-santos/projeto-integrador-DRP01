0779dcf0327318b795cff4898f829365d6155934
ivani santos <ivanisandre@outlook.com>
2026-04-26 21:21:33 -0300

    Metodos get e post sem banco de dados

--- website/auth.py
+++ website/auth.py
@@ -2,11 +2,11 @@ from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login-fotografo')
+@auth.route('/login-fotografo', methods= ['GET', 'POST'])
 def login_fotografo():
     return render_template("login_fotografo.html")
 
-@auth.route('/login-client')
+@auth.route('/login-client', methods= ['GET', 'POST'])
 def login_client():
     return render_template("login_client.html")
 
@@ -14,10 +14,10 @@ def login_client():
 def logout():
     return "<p>Sair</p>"
 
-@auth.route('/signup-fotografo')
+@auth.route('/signup-fotografo', methods= ['GET', 'POST'])
 def signup_fotografo():
     return render_template("signup_fotografo.html")
 
-@auth.route('/signup-client')
+@auth.route('/signup-client', methods= ['GET', 'POST'])
 def signup_client():
     return render_template("signup_client.html")
